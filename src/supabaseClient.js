2148212d592b15c712e5f4e0c529f79959a1e0b3
Profesor La Salle <cpenar@lasalle.cat>
2026-02-10 12:33:20 +0100

    Test: Hardcode Supabase credentials

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://zvzmgywaoecfyqksjwb.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2em1neXdhb2VjZnlxa3Nqd2IiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTczNDU1MzgxMCwiZXhwIjoyMDUwMTI5ODEwfQ.NlbD0uG8qxQ7_cNHdIa8FXEPbvgdCOH_pLkAKZoN9MA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
