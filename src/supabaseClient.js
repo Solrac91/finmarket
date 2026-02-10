9a2e6e49420dd9d3927080d75edc5886c265d78a
Profesor La Salle <cpenar@lasalle.cat>
2026-02-10 12:58:32 +0100

    Add auth config with PKCE flow to Supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+﻿import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zvzmgywaoecfyqksjwb.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2em1neXdhb2VjZnlxa3Nqd2IiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTczNDU1MzgxMCwiZXhwIjoyMDUwMTI5ODEwfQ.NlbD0uG8qxQ7_cNHdIa8FXEPbvgdCOH_pLkAKZoN9MA'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+    flowType: 'pkce'
+  }
+})
